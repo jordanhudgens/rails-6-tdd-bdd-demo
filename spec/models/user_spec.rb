44066d06d85c0d63cbbf0201f1df73a0bf602a7b
Jordan Hudgens <jordan@devcamp.com>
2020-09-23 13:14:07 -0700

    Updated user model and specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Creation" do
+    it "saves with correct data attributes" do
+      email = "user@example.com"
+      user = User.create!(email: email, password: "asdfasdf")
+      expect(user.email).to eq(email)
+    end
+  end
 end
